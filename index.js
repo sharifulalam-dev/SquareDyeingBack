371c6e0803d1e44c980d0fd342dc026534f6337a
sharifulalam-dev <sharifulalam.tex@gmail.com>
2025-01-31 12:25:36 +0600

    cors update done

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ const port = process.env.PORT || 9000;
 const app = express();
 
 const corsOptions = {
-  origin: ["http://localhost:5173", "http://localhost:5174"],
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "http://squaresmfl.netlify.app",
+  ],
 };
 app.use(cors(corsOptions));
 
